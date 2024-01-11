228166f5702c4eceaa5250cd89a55ba05f77ae26
Coltrane Willsey <cwillsey06@gmail.com>
2024-01-11 10:44:09 -0700

    Apply `gruvbox` patch

--- config.def.h
+++ config.def.h
@@ -96,32 +96,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#282828", /* black   */
-	"#901b15", /* red     */
-	"#787427", /* green   */
-	"#ac792e", /* yellow  */
-	"#2c6476", /* blue    */
-	"#85436f", /* magenta */
-	"#507a5b", /* cyan    */
-	"#786e64", /* white   */
+	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#cc241d", /* red     */
+	[2] = "#98971a", /* green   */
+	[3] = "#d79921", /* yellow  */
+	[4] = "#458588", /* blue    */
+	[5] = "#b16286", /* magenta */
+	[6] = "#689d6a", /* cyan    */
+	[7] = "#a89984", /* white   */
 
 	/* 8 bright colors */
-	"#3b3837", /* black   */
-	"#bc3629", /* red     */
-	"#989736", /* green   */
-	"#ce9c3e", /* yellow  */
-	"#558387", /* blue    */
-	"#a66684", /* magenta */
-	"#739c6f", /* cyan    */
-	"#908375", /* white   */
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#a5cdff",
-	"#282828", /* default foreground colour */
-	"#ffffff", /* default background colour */
+	[8]  = "#928374", /* black   */
+	[9]  = "#fb4934", /* red     */
+	[10] = "#b8bb26", /* green   */
+	[11] = "#fabd2f", /* yellow  */
+	[12] = "#83a598", /* blue    */
+	[13] = "#d3869b", /* magenta */
+	[14] = "#8ec07c", /* cyan    */
+	[15] = "#ebdbb2", /* white   */
 };
 
 
@@ -129,9 +121,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
